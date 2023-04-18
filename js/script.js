ad62ac45e697902250523b7cc59d1d1faec618f4
ceciaups <cecilia127@msn.com>
2023-04-17 22:30:00 -0400

    Add toggle filter

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
+window.onload = function() {
+  let choice = document.getElementsByClassName("filter-choice");
+  for (let i = 0; i < choice.length; i++) {
+    choice[i].addEventListener('click', toggleChoice);
+  }
+}
+
+var filters = [];
+
 function toggleFilter() {
   let filter = document.getElementById("support-filter");
   filter.classList.toggle("filter-show");
+}
+
+function cancelFilter() {
+  toggleFilter();
+
+  // switch the choices back
+  let choice = document.getElementsByClassName("filter-choice");
+  for (let i = 0; i < choice.length; i++) {
+    if (filters.includes(i)) {
+      if (!choice[i].classList.contains("filter-checked")) {
+        choice[i].classList.add("filter-checked");
+      }
+    }
+    else {
+      if (choice[i].classList.contains("filter-checked")) {
+        choice[i].classList.remove("filter-checked");
+      }
+    }
+  }
+}
+
+function saveFilter() {
+  // save the choices
+  filters = [];
+  let choice = document.getElementsByClassName("filter-choice");
+  for (let i = 0; i < choice.length; i++) {
+    if (choice[i].classList.contains("filter-checked")) {
+      filters.push(i);
+    }
+  }
+  toggleFilter();
+}
+
+function toggleChoice() {
+  this.classList.toggle("filter-checked");
 }
